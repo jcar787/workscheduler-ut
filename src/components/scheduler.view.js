fbc50a8294d666a57c4aa1edba062ea0064902d6
Jeff C <blackone787@gmail.com>
2020-05-24 15:58:38 -0500

    Adding rows to the view. Need to create a custom component to handle the rows individually.

--- src/components/scheduler.view.js
+++ src/components/scheduler.view.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+const isHourMorning = (time) => {
+  return time < 12;
+};
+
 const SchedulerView = (props) => {
-  return <div>This is my scheduler view</div>;
+  const { todaysDate, hours, onClickWrapper } = props;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <h1>{todaysDate}</h1>
+        </div>
+        <div className="col-12">
+          {hours.map((hour, i) => {
+            const showHour = `${
+              isHourMorning(hour.time) ? hour.time : hour.time - 12
+            }:00 ${isHourMorning(hour.time) ? 'AM' : 'PM'}`;
+            return (
+              <div key={i}>
+                {showHour} - {hour.text}
+                <button onClick={onClickWrapper(hour)}>
+                  Temporary Save Button
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SchedulerView;
